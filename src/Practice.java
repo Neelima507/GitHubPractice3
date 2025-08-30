db543a7327a9b0d5dc2875c69b66a45f0fa7636e
Neelima <neelutgd507@gmail.com>
2025-08-30 16:07:50 -0700

    added first commit

---
+++ src/Practice.java
@@ -0,0 +1,18 @@
+
+
+
+
+public class Practice {
+	public void newmethod() {
+		
+	}
+		
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("World");
+		System.out.println("AGAIN");
+
+	}
+
+}
